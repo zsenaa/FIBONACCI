c1297b981973c227e332b4b6f8a5c33a1c0a3148
Sena <zhrsnkunt@gmail.com>
2023-03-24 18:07:36 +0300

    FIBONACCI
    No matter how many times I type into a textbox, when the button is pressed, I should see the current Fibonacci number on the screen.

---
+++ FİBONACCİ/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FİBONACCİ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static long fibonacci1(int sayi)
+        {
+            if (sayi == 0 || sayi == 1)
+                return 1;
+            else
+            {
+                return fibonacci1(sayi - 1) + fibonacci1(sayi - 2);
+            }
+
+        }
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(guna2TextBox3.Text);
+            guna2TextBox1.Text = fibonacci1(sayi).ToString();
+        }
+    }
+}
